be19b712d4ae6d356712cd49f6b26032c1be0c9c
AhmedMohamed040 <a.mohamed00019@gmail.com>
2024-11-14 09:34:21 +0200

    feat:page

--- src/app/dashboard/banners/[bannerId]/page.tsx
+++ src/app/dashboard/banners/[bannerId]/page.tsx
@@ -13,7 +13,7 @@ type IProps = {
 };
 const Page = async ({ params, searchParams }: IProps) => {
   const page = typeof searchParams?.page === 'string' ? Number(searchParams?.page) : 1;
-  const limit = typeof searchParams?.limit === 'string' ? Number(searchParams?.limit) : 6;
+  const limit = typeof searchParams?.limit === 'string' ? Number(searchParams?.limit) : 5;
 
   const bannerDetails = await fetchSingleBannder(params.bannerId);
   const bannerCenters = await fetchSingleBannderCenters(params.bannerId, page, limit);
